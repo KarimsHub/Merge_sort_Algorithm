61c7ccb2582a6131c66e89b0f4456e538b795d7f
Karim Wölbert <karimwolbert@MacBook-Pro.fritz.box>
2020-08-31 15:17:18 +0200

    added comments

--- merge_sort_algorithm.py
+++ merge_sort_algorithm.py
@@ -9,41 +9,41 @@ def random_numbers_generator():
     return l
 
 unsorted_l = random_numbers_generator()
-
+print(unsorted_l)
 start = time.time()
 
 def merge(list, l, m, r):
-    divider = m + 1
-    l_cache = l
+    divider = m + 1 # to see there the right list starts
+    l_cache = l # to see there the left has started
     out_list = []
 
-    for i in range(r - l + 1):
+    for i in range(r - l + 1): # sorting the amount of elemnts in our list
         if l > m:
-            out_list.append(list[divider])
+            out_list.append(list[divider]) # there are no more elements in the left list
             divider = divider + 1
         elif divider > r: # we would be out of the list when the divider is bigger than r
-            out_list.append(list[l])
+            out_list.append(list[l]) # there are no more elements in the right list
             l = l + 1
-        elif list[l] < list[divider]:
+        elif list[l] < list[divider]: # list[l] is first element of the left list and list[divider] the first element of the right list
             out_list.append(list[l]) # appending the smaller element to the new merge list
             l = l + 1 # moving to next element in the left part
         elif list[l] >= list[divider]:
             out_list.append(list[divider])
             divider = divider + 1 # have to move to the next element in the divider list
 
-    for i in range(len(out_list)):
+    for i in range(len(out_list)): # ierating through the elements of the already sorted outlist
         list[i + l_cache] = out_list[i]
 
 
-def merge_sort(unsorted_list, l = 0, r = None):
+def merge_sort(unsorted_list, l = 0, r = None): # setting the default values for l and r
     if r is None:
-        r = len(unsorted_list) - 1
+        r = len(unsorted_list) - 1 # have to use this workaround to set the lenght of the list as parameter
     
     if l < r: # we only want the algorithm work if the left edge is smaller
         m = (l + r - 1) // 2 # defining the middle element of the list
-        merge_sort(unsorted_list, l, m) # left part of the original list is sorted, using the list itself to work inplace
-        merge_sort(unsorted_list, m + 1, r) # right part of the original list ist sorted
-        merge(unsorted_list, l, m, r)
+        merge_sort(unsorted_list, l, m) # using function to define the left part of the unsorted list
+        merge_sort(unsorted_list, m + 1, r) # using function to define the right part of the unsorted list
+        merge(unsorted_list, l, m, r) # using function to combine the parts 
 
 merge_sort(unsorted_l)
 #print(unsorted_l)
